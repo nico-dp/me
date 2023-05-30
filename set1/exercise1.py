1a0c0f3fcea805c5407aba6e0df0b5d163c09da6
Nico de Paulis <depaulis.nico@gmail.com>
2023-05-30 20:35:47 +1000

    1st commit

--- set1/exercise1.py
+++ set1/exercise1.py
@@ -7,6 +7,6 @@ Look on the course website for how to do this, and for how to run the tests.
 https://design-computing.github.io/md/week1
 
 """
-# 👇👇 Write your code here 👇👇
-
+# 👇👇" Write your code here 👇👇
+print("Hello world!")
 # 👆👆 Write your code here 👆👆
