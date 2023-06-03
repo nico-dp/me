3ba1a4f0dec5bdaa0bf801c04f272da78a60e98e
Nico de Paulis <depaulis.nico@gmail.com>
2023-06-03 18:10:31 +1000

    ex0 and 1 of set 2

--- set2/exercise1.py
+++ set2/exercise1.py
@@ -19,18 +19,27 @@ print("hello! Let's get started")  # it printed "hello! Let's get started"
 
 some_words = ["what", "does", "this", "line", "do", "?"]
 
+# I think that this will go through the list of words and print each word
 for word in some_words:
     print(word)
+# It went through the list of and printed each word
 
+# I think that this will do the same thing as x and word are both variables
 for x in some_words:
     print(x)
+# This also printed every word in the list, although in the debugger it shows word: '?' and im not sure why
 
+# This will print the entire list some_words
 print(some_words)
+# This printed the entire list some_words, in the debugger it once again shows word: '?' and now x: '?'.
+# I know think that this is because the debugger sees the variables above but has nothing to do with them
 
+# I think that len stands for length and therefore if the list has more than the words it will print "some_words contains more than 3 words"
 if len(some_words) > 3:
     print("some_words contains more than 3 words")
+# It printed "some_words contains more than 3 words"
 
-
+# Well according to google, this will print my computers; system, node, release, version, machine, and processor
 def usefulFunction():
     """
     You may want to look up what uname does before you guess
@@ -41,3 +50,4 @@ def usefulFunction():
 
 
 usefulFunction()
+# I guess it did this?
