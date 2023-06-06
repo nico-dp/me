26783afeac341d68e2f924e5e8f58a67b9f6380b
Nico de Paulis <depaulis.nico@gmail.com>
2023-06-06 22:17:51 +1000

    ex3 commit

--- set2/exercise3.py
+++ set2/exercise3.py
@@ -13,8 +13,12 @@ def is_odd(a_number):
 
     So if a_number modulo two is zero, then it's even.
     """
-    return None
 
+    if a_number % 2 == 0:
+        return False
+    
+    else: 
+        return True
 
 def fix_it(moves=True, should_move=True):
     """Decide what to do.
@@ -31,7 +35,14 @@ def fix_it(moves=True, should_move=True):
     Most people write this function with 4 return statements.
     As an extra challenge, see if you can get that down to three.
     """
-    return None
+    if moves and not should_move:
+        return "Duct Tape"
+    
+    elif not moves and should_move:
+        return "WD-40"
+    
+    else:
+        return "No Problem"
 
 
 def loops_preview():
@@ -54,7 +65,11 @@ def loops_1a():
     return a list of 10 items, each one a string with exacly one star in it.
     E.g.: ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
     """
-    return None
+
+    star_list = []
+    for i in range(10):
+        star_list.append("*")
+    return star_list
 
 
 def loops_1c(number_of_items=5, symbol="#"):
@@ -67,7 +82,10 @@ def loops_1c(number_of_items=5, symbol="#"):
     Remember that you're being passed arguments here. Don't hard code the number
     or the symbol, let it be whatever it wants to be.
     """
-    return None
+    symbol_list = []
+    for i in range(number_of_items):
+        symbol_list.append(symbol)
+    return symbol_list
 
 
 def loops_2():
@@ -88,8 +106,22 @@ def loops_2():
             ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*'],
           ]
     """
-    return None
+    """
+    star_square = []
+    for k in range (10):
+        star_list = []
+        for i in range(10):
+            star_list.append("*")
+        star_square.append(star_list)
+    
+    return star_square
+    """
 
+    star_square = []
+    for k in range(10):
+        star_square.append(loops_1c(number_of_items=10, symbol="*"))
+
+    return star_square
 
 def loops_3():
     """Make a rising block of numbers.
@@ -112,7 +144,14 @@ def loops_3():
     TIP: notice that this needs to to return strings of numbers,
          so call str(number) to cast.
     """
-    return None
+    num_square = []
+    for k in range(10):
+        num_row = []
+        for i in range(10):
+            num_row.append(str(k))
+        num_square.append(num_row)
+    
+    return num_square
 
 
 def loops_4():
@@ -132,7 +171,14 @@ def loops_4():
       ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     ]
     """
-    return None
+    num_square = []
+    for k in range(10):
+        num_row = []
+        for i in range(10):
+            num_row.append(str(i))
+        num_square.append(num_row)
+    
+    return num_square
 
 
 def loops_5():
@@ -162,7 +208,14 @@ def loops_5():
         f"There are {num_bottles} green bottles"
     you'll come to see the pros and cons of each over time.
     """
-    return None
+
+    block = []
+    for i in range(10):
+        row = []
+        for j in range(5):
+            row.append("(i" + str(i) + ", j" + str(j) + ")")
+        block.append(row)
+    return block
 
 
 def loops_6():
@@ -185,7 +238,15 @@ def loops_6():
     You can use a variable.
     TIP: look out for the starting condition.
     """
-    return None
+
+    wedge = []
+    for k in range(10):
+        row = []
+        for i in range(k + 1):
+            row.append(str(i))
+        wedge.append(row)
+    
+    return wedge
 
 
 def loops_7():
@@ -209,6 +270,8 @@ def loops_7():
     This is a hard problem. Use lots of experimentation and draw
     lots of diagrams!
     """
+
+    
     return None
 
 
