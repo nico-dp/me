b2d044484dc2fbd4f73209224298a8b8d618adff
Nico de Paulis <depaulis.nico@gmail.com>
2023-07-19 12:30:42 +1000

    ex1

--- set5/exercise1.py
+++ set5/exercise1.py
@@ -25,6 +25,7 @@ Modify this function, don't write a whole new one.
 
 
 def wordy_pyramid():
+    """
     baseURL = (
         "https://us-central1-waldenpondpress.cloudfunctions.net/"
         "give_me_a_word?wordlength={length}"
@@ -46,19 +47,43 @@ def wordy_pyramid():
             pyramid_list.append(message)
         else:
             print("failed a request", r.status_code, i)
+    """
+
+    up = list_of_words_with_lengths(3, 21, 2)
+    down = list_of_words_with_lengths(20, 3, -2)
+
+    pyramid_list = up + down
 
     return pyramid_list
 
 
+
 def get_a_word_of_length_n(length):
-    pass
+    URL = f'https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength={length}'
+    
+    response = requests.get(URL)
+
+    if response.status_code == 200:
+        word = response.text
+    else:
+        print("failed a request", response.status_code)
+    
+    return word
 
 
-def list_of_words_with_lengths(list_of_lengths):
-    pass
+def list_of_words_with_lengths(start, end, step):
+    list_of_words = []
+
+    for i in range(start, end, step):
+        word = get_a_word_of_length_n(i)
+        list_of_words.append(word)
+
+    return list_of_words
 
 
 if __name__ == "__main__":
     pyramid = wordy_pyramid()
     for word in pyramid:
         print(word)
+    
+    list_of_words_with_lengths(3, 21, 2)
